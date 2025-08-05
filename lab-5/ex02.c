1bf967ce5bf798310ac6026b580a5122fc91f9d8
TK-Rin <thiradet.kur@gmail.com>
2025-08-05 03:55:30 +0000

    done ex04

--- lab-5/ex02.c
+++ lab-5/ex02.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-    int original[] = {1, 2, 3, 4, 5, 6, 7, 8, 9}, i,n = 9;
+    int original[] = {1, 2, 3, 4, 5, 6, 7, 8, 9},i,n = 9;
 
     printf("Reversed Array: ");
     for(i=n-1;i>=0;i--) 
