b343ccfb9597dbe0aa210a317598f0cf1c25679d
TK-Rin <thiradet.kur@gmail.com>
2025-08-05 03:50:25 +0000

    Lab-5 ex3 done

---
+++ lab-5/ex02.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int original[] = {1, 2, 3, 4, 5, 6, 7, 8, 9}, i,n = 9;
+
+    printf("Reversed Array: ");
+    for(i=n-1;i>=0;i--) 
+    {
+        printf("%d", original[i]);
+        if(i > 0) printf(", ");
+    }
+}
---
+++ lab-5/ex04.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int values[10],i;
+    for(i = 0; i < 10; i++) 
+    {
+        printf("Enter the value %d here: ", i + 1);scanf("%d", &values[i]);
+    }
+}
