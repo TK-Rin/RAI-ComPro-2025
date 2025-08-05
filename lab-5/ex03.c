a30689e192234b262886b97ad998ad0493502252
TK-Rin <thiradet.kur@gmail.com>
2025-08-05 04:08:21 +0000

    Lab-5; Ex04 done

--- lab-5/ex03.c
+++ lab-5/ex03.c
@@ -1,11 +1,14 @@
 #include<stdio.h>
 int main()
 {
-    int n=5,i,tm= 0,hm=0,m[5];
+    int n=5,i,tm=0,hm=0,m[5];
     for(i = 0; i < n; i++) {
         printf("Enter the marks of student %d: ", i+1);scanf("%d",&m[i]);
-        tm += m[i];
-        if(i == 0 || m[i] > hm) hm = m[i];
+        tm += m[i]; // tm = tm + m[i]
+        if(i == 0 || m[i] > hm) 
+        {
+            hm = m[i];
+        }
     }
     printf("Total Marks : %d\n", tm);
     printf("Highest Marks: %d\n", hm);
--- lab-5/ex04.c
+++ lab-5/ex04.c
@@ -1,9 +1,18 @@
 #include<stdio.h>
 int main()
 {
-    int values[10],i;
-    for(i = 0; i < 10; i++) 
+    int v[10],i,e=0,o=0;
+    for(i=0;i<10; i++) 
     {
-        printf("Enter the value %d here: ", i + 1);scanf("%d", &values[i]);
+        printf("Enter value %d: ", i+1);
+        scanf("%d", &v[i]);
     }
+    for(i = 0; i < 10; i++) {
+        if(v[i] % 2 == 0)
+            e++;
+        else
+            o++;
+    }
+    printf("Even numbers: %d\n", e);
+    printf("Odd numbers: %d\n", o);
 }
