d59c4a274da28911793566d225ec4ef8516060d8
TK-Rin <thiradet.kur@gmail.com>
2025-09-15 12:49:58 +0000

    hw done

--- lab-7/hw01.c
+++ lab-7/hw01.c
@@ -7,11 +7,32 @@ int main()
     int *pA = array;
     for (int i = 0; i < 9; i++)
     {
-        printf("%d\n", *pA);
+        printf("%d", *pA);
         pA++;
     }
-    printf("--------------\n");
+    printf("\n--------------\n");
+
+    float x = 1.1;
+	float y = 2.2;
+    float *pX = &y, *pY = &x;
+    printf("x=%f\ty=%f1",*pX,*pY);
+    printf("\n--------------\n");
+
+    struct S {
+    int a, b, c;
+    };
     
+    struct S s;
+    int *pC = &s.c;
+
+    printf("Enter first number: ");
+    scanf("%d", &s.a);
+    printf("Enter second number: ");
+    scanf("%d", &s.b);
+
+    *pC = s.a + s.b;
+
+    printf("Sum = %d\n", s.c);
 
     return 0;
 }
